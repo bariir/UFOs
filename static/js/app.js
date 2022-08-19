a3e9b7ca5ed5dacdadc0739a0432f95a3683875d
misse <mbariir76@gmail.com>
2022-08-18 18:29:45 -0700

    Updated app.js file

--- static/js/app.js
+++ static/js/app.js
@@ -26,3 +26,30 @@ data.forEach((dataRow) => {
   });
 
 }
+
+function hanleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+
+    // Check to see if a date was entered and filter teh 
+    // data using that date.
+    if (date){
+        //Apply `filter` to the table data to only keep the
+        // rows where teh `datetime` value matches teh filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData
+    buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", hanleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
